fe435504a23d09568228aed38460873c6c2fe66a
SuinCho <suin.cho@stonybrook.edu>
2019-05-22 20:58:01 +0900

    로그인 바꿈

--- login.php
+++ login.php
@@ -7,15 +7,15 @@
 
 <body>
         <div align='center'>
-        <span>로그인</span>
+        <span>Log In</span>
 
         <form method='get' action='login_action.php'>
                 <p>ID: <input name="id" type="text"></p>
                 <p>PW: <input name="pw" type="password"></p>
-                <input type="submit" value="로그인">
+                <input type="submit" value="Log In">
         </form>
         <br />
-        <button id="join" onclick="location.href='./join.php'">회원가입</button>
+        <button id="join" onclick="location.href='./join.php'">Sign Up</button>
 
         </div>
 
