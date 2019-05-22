778f83fc3645c85f6db760cd8d806b065606a785
SuinCho <suin.cho@stonybrook.edu>
2019-05-22 21:00:34 +0900

    회원가입 영어로 바꿈

--- join.php
+++ join.php
@@ -7,12 +7,12 @@
 <body>
 
         <div align="center">
-                <p>회원가입</p>
+                <p>Sign Up</p>
                 <form method='get' action='join_action.php'>
                         <p>ID: <input type="text" name="id"></p>
                         <p>PW: <input type="password" name="pw"></p>
                         <p>Email: <input type="email" name="email"></p>
-                        <input type="submit" value="회원가입">
+                        <input type="submit" value="Sign Up">
                 </form>
         </div>
 </body>
