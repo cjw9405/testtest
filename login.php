2c0a90c2d04de2d7495d49402156130f509c7d91
SuinCho <suin.cho@stonybrook.edu>
2019-05-25 22:30:26 +0900

    login고침

--- login.php
+++ login.php
@@ -7,9 +7,7 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     header("location: welcome.php");
     exit;
 }
- 
-// Include config file
-require_once "config.php";
+
 
 // Define variables and initialize with empty values
 $username = $password = "";
