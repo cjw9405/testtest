2cdac71e9abc9c03e55425edc0c714d3fca5aef3
TreeAana <sg04108@gmail.com>
2019-06-01 18:49:10 +0900

    완성된거입니다

--- deleteVehice.php
+++ deleteVehice.php
@@ -3,45 +3,126 @@
 
 <head>
 <title>PHP and MySQL</title>
+<style>
+
+
+
+.main_container {
+
+width:100%;
+
+height:100%;
+}
+
+
+
+.main_title {
+
+width:100%;
+
+height:20%;
+
+float:left;
+
+
+text-align: center;
+border-bottom-width:thin;
+border-bottom:solid;}
+
+
+
+.main_left_btn {
+
+width:35%;
+
+height:80%;
+
+
+text-align: center;
+
+float:left;}
+
+.main_mid_btn {
+
+width:30%;
+
+height:80%;
+text-align: center;
+
+
+
+float:left;}
+
+
+
+.main_right_btn {
+
+width:35%;
+
+height:80%;
+
+
+text-align: center;
+
+float:left;}
+
+
+
+
+
+</style>
+
+
+
 </head>
 
 <body>
 
-<h1>Delete the Vehicle</h1>
-<?php // adddelete.php
-  require_once 'accessDatabase.php';
- //session_start();
-// $id = $_SESSION['']
+<div class="main_container">
 
-if (isset($_POST['delete']) &&
-    isset($_POST['vid'])) {
+<div class="main_title">
+  <h1>Delete the Vehicle</h1>
 
-  $vid  = get_post($conn, 'vid');
+  <?php
+   // adddelete.php
+    require_once 'accessDatabase.php';
+   //session_start();
+  // $id = $_SESSION['']
 
-  $query  = "SELECT * FROM Rent WHERE vid='$vid'";
-  $result = $conn->query($query);
-  if($result->num_rows >0){
-    echo "The vehicle can not be removed because some customer has already rented it.<br><br>";
-  }else{
-    $query  = "DELETE FROM Vehicle WHERE vid='$vid'";
+  if (isset($_POST['delete']) &&
+      isset($_POST['vid'])) {
+
+    $vid  = get_post($conn, 'vid');
+
+    $query  = "SELECT * FROM Rent WHERE vid='$vid'";
     $result = $conn->query($query);
-    if (!$result){
-        echo "DELETE failed: $query<br>" . $conn->error . "<br><br>";}
-    else{
-       echo "DELETE Sucess: $query<br>" . $conn->error . "<br><br>";}
+    if($result->num_rows >0){
+      echo "The vehicle can not be removed because some customer has already rented it.<br><br>";
+    }else{
+      $query  = "DELETE FROM Vehicle WHERE vid='$vid'";
+      $result = $conn->query($query);
+      if (!$result){
+          echo "DELETE failed: $query<br>" . $conn->error . "<br><br>";}
+      else{
+         echo "DELETE Sucess: $query<br>" . $conn->error . "<br><br>";}
+    }
+
+
   }
+?>
+<li><a href="adminwebpage.php">Go to the Index </a></li><br>
+</div>
 
+<div class="main_left_btn">
+  <?php
+  echo "Motorcycles";
 
-}
-echo "Motorcycles";
+  $query  = "SELECT * FROM Motorcycle";
+  $result = $conn->query($query);
+  if (!$result) die ("Database access failed: " . $conn->error);
 
-$query  = "SELECT * FROM Motorcycle";
-$result = $conn->query($query);
-if (!$result) die ("Database access failed: " . $conn->error);
-
-$rows = $result->num_rows;
-for ($j = 0 ; $j < $rows ; ++$j) {
-  $result->data_seek($j);
+  $rows = $result->num_rows;
+  for ($j = 0 ; $j < $rows ; ++$j) {$result->data_seek($j);
   $row = $result->fetch_array(MYSQLI_NUM);
 
   echo <<<_END
@@ -56,8 +137,13 @@ for ($j = 0 ; $j < $rows ; ++$j) {
 <input type="hidden" name="vid" value="$row[0]">
 <input type="submit" value="DELETE RECORD"></form>
 _END;
-}
 
+  }
+?>
+
+</div>
+
+<div class="main_mid_btn">  <?php
 echo "Tank";
 
 $query  = "SELECT * FROM Tank";
@@ -82,7 +168,11 @@ for ($j = 0 ; $j < $rows ; ++$j) {
 <input type="submit" value="DELETE RECORD"></form>
 _END;
 }
-echo "Car";
+
+?></div>
+
+<div class="main_right_btn"> <?php
+echo "Tank";
 
 $query  = "SELECT * FROM Car";
 $result = $conn->query($query);
@@ -95,11 +185,11 @@ for ($j = 0 ; $j < $rows ; ++$j) {
 
   echo <<<_END
 <pre>
-  vid $row[0]
-  type $row[1]
-  color $row[2]
-  speed $row[3]
-  enginecapacity $row[4]
+vid $row[0]
+type $row[1]
+color $row[2]
+speed $row[3]
+enginecapacity $row[4]
     </pre>
 <form action="deleteVehice.php" method="post">
 <input type="hidden" name="delete" value="yes">
@@ -107,10 +197,6 @@ for ($j = 0 ; $j < $rows ; ++$j) {
 <input type="submit" value="DELETE RECORD"></form>
 _END;
 }
-
-
-
-
 
 
 $conn->close();
@@ -121,9 +207,13 @@ function get_post($conn, $var) {
   return $conn->real_escape_string($_POST[$var]);
 }
 
-?>
 
 
-<li><a href="adminwebpage.php">Go to the Index </a></li><br>
+?></div>
+
+
+</div>
+
 </body>
+
 </html>
