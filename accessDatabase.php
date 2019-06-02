513d06579aa777d570e78fcfa1e028e7de869f08
SuinCho <suin.cho@stonybrook.edu>
2019-06-03 00:03:13 +0900

    modified some files

--- accessDatabase.php
+++
@@ -1,8 +0,0 @@
-<?php   // login.php
-  $hn = 'localhost';
-  $db = 'hlps5';
-  $un = 'grader';
-  $pw = 'allowme';
-  $conn = new mysqli($hn, $un, $pw, $db);
-  if ($conn->connect_error) die($conn->connect_error);
-?>
